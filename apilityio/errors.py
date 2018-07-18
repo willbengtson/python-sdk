023904e2909e00e0c37978793d7b5b0d2b15c656
Diego Parrilla <diego.parrilla.santamaria@gmail.com>
2018-07-18 11:44:44 +0200

    Removed conflict char for 2.7 builds

--- apilityio/errors.py
+++ apilityio/errors.py
@@ -1,5 +1,5 @@
 """
-Copyright 2017-2018 CAPITAL LAB OÜ
+Copyright 2017-2018 CAPITAL LAB OU
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
